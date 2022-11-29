9f6e5516b4ef02fb1fb8ab456e64b9c9a2fc7a34
Indraan S Toor <indraantoor@Indraans-MacBook-Pro.local>
2022-11-29 11:34:41 +0530

    fix
    Signed-off-by: Indraan S Toor <indraantoor@Indraans-MacBook-Pro.local>

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,15 +1,14 @@
 export default function handler(req: any, res: any) {
+  console.log(req.method);
   const requestMethod = req.method;
-  if (req.body) {
-    var body = JSON.parse(req.body);
-  }
+  var body = JSON.parse(req.body);
   switch (requestMethod) {
-    case "POST":
-      res
-        .status(200)
-        .json({
-          message: `You submitted the following data: ${JSON.stringify(body)}`,
-        });
+    case "POST": {
+      res.status(200).json({
+        message: `You submitted the following data: ${JSON.stringify(body)}`,
+      });
+      break;
+    }
 
     // handle other HTTP methods
     default:
